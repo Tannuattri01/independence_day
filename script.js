ef47afe1cfa392d39c08e1287f5000160ded68aa
Tannuattri01 <161629643+Tannuattri01@users.noreply.github.com>
2024-08-11 17:56:01 +0530

    initial files are added

---
+++ script.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+    reset: true ,
+    distance:"80px" ,
+    duration:2000,
+    delay:200
+   });
+  
+   ScrollReveal().reveal('.', { origin: 'top' });
+   ScrollReveal().reveal('.august,.day', { origin: 'bottom' });
+   ScrollReveal().reveal('.img1', { origin: 'left' });
+   ScrollReveal().reveal('.img2', { origin: 'right' });
+
